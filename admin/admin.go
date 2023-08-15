04ef8ac7b02d6d4a33f744fd041ee0bb3e611f8b
Pera Urosevic <pera.urosevic@gmail.com>
2023-08-15 19:10:48 +0300

    display admin messages

--- admin/admin.go
+++ admin/admin.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"dreamsleep.cloud/micron/storage"
+	"dreamsleep.cloud/micron/system"
 	"dreamsleep.cloud/micron/ticker"
 )
 
@@ -14,12 +15,20 @@ func Start() {
 		char, _ := reader.ReadByte()
 		switch char {
 		case 'q':
+			system.Log("Quiting...")
 			os.Exit(0)
 		case 'p':
 			ticker.Running = !ticker.Running
+			if ticker.Running {
+				system.Log("Resumed")
+			} else {
+				system.Log("Paused")
+			}
 		case 'e':
 			ticker.Running = false
+			system.Log("Editing config")
 			storage.ConfigEdit()
+			system.Log("Resumed")
 			ticker.Running = true
 		}
 	}
