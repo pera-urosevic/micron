8ec4720fec8d33759e0fdb57c9b2e81749630b7e
Pera Urosevic <pera.urosevic@gmail.com>
2024-06-15 11:13:49 +0300

    log time format

--- system/log.go
+++ system/log.go
@@ -5,8 +5,12 @@ import (
 	"time"
 )
 
+func TimeFormat(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05")
+}
+
 func Log(args ...any) {
-	timestamp := fmt.Sprintf("[%s]", time.Now().Format("2006-01-02 15:04:05"))
+	timestamp := fmt.Sprintf("[%s]", TimeFormat(time.Now()))
 	line := append([]any{timestamp}, args...)
 	fmt.Println(line...)
 }
